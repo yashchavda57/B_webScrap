887314d0510ef7158e0936e75a102d03e9987299
yashchavda57 <chavdayash1709@gmail.com>
2020-09-19 18:07:00 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import bs4
+
+url=input("Enter your url: ")
+response = requests.get(url)
+#print(type(response))
+#print(response.text)
+filename="temp.html"
+bs = bs4.BeautifulSoup(response.text,"html.parser")
+
+formatted_text = bs.prettify()
+print(formatted_text)
+
+with open(filename,"w+") as f:
+    f.write(formatted_text)
+
+list_div = bs.find_all('div')    
+print(list_div)
+no_of_div = len(list_div)
+
+list_as = bs.find_all('a')
+no_of_as = len(list_as)
+
+print("Number of content :",list_div)
+print("Number of as :", no_of_as)
